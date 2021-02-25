9ef777695249ee441dd2966c3676c907af36d1a5
Tristan Nelson <Tristan@tristans-mbp.lan>
2021-02-25 14:15:30 -0500

    info component added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import { useState } from "react";
 import Header from "./components/Header.js";
 import Map from "./components/Map";
 import MovementButtons from "./components/MovementButtons";
+import Info from "./components/Info";
 
 function App() {
   const [center, setCenter] = useState([43.88, -72.7317]);
   //viewcenter is the current center being viewed
   const [viewCenter, setViewCenter] = useState([43.88, -72.7317]);
 
-  console.log(`insde app.js view center is being accessed and is ${viewCenter}`)
+  console.log(
+    `inside app.js view center is being accessed and is ${viewCenter}`
+  );
 
   return (
     <>
       <Header />
       <div id="map-container">
-        <Map center={center} viewCenter={viewCenter}/>
-        <MovementButtons center={center} viewCenter={viewCenter} setCenter={setCenter} setViewCenter={setViewCenter}/>
+        <Map center={center} viewCenter={viewCenter} />
+        <MovementButtons
+          center={center}
+          viewCenter={viewCenter}
+          setCenter={setCenter}
+          setViewCenter={setViewCenter}
+        />
+        <Info />
       </div>
     </>
   );
---
+++ src/components/Info.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+
+const Info = (props) => {
+  return (
+    <div id="info-wrapper">
+      <div id="info">Placeholder Info Box</div>
+    </div>
+  );
+};
+
+export default Info;
