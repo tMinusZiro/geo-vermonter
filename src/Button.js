9a6eb9ce002a8958a2eb2bf85bcb2134f9f0441e
Jack Brooks <cyrusandethan@yahoo.com>
2021-02-25 15:10:02 -0500

    Play my game

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+<h1>
+<button name = "button" onclick = "event( )">
+    Enter
+</button>
+
+<button name = "button" onclick = "event( )">
+    Exit
+</button>
+
+<button name = "button" onclick = "event( )">
+    quit
+</button>
+</h1>
